f0669a226ea7ba07e3b576a1582a7b5f4382d6d4
Md Rakibul Islam <bdsky25@gmail.com>
2024-01-21 05:15:46 +0600

    Implement Bistro Boss Section on home page and add Cinzel font

---
+++ src/pages/Home/BistroBoss/BistroBoss.jsx
@@ -0,0 +1,26 @@
+import bistroBg from '../../../assets/home/chef-service.jpg';
+
+const BistroBoss = () => {
+    return (
+        <section
+            className='bg-fixed bg-cover bg-center bg-no-repeat py-28'
+            style={{ backgroundImage: `url(${bistroBg})` }}
+        >
+            <div className='container mx-auto px-5'>
+                <div className=' bg-white lg:w-9/12 p-20 mx-auto'>
+                    <h2 className="text-neutral-900 text-center text-[30px] sm:text-[45px] font-normal font-['Cinzel']">
+                        Bistro Boss
+                    </h2>
+                    <p className="text-center lg:w-5/6 mx-auto text-neutral-900 text-sm sm:text-base font-normal font-['Inter'] leading-relaxed">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus,
+                        libero accusamus laborum deserunt ratione dolor officiis praesentium!
+                        Deserunt magni aperiam dolor eius dolore at, nihil iusto ducimus incidunt
+                        quibusdam nemo.
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default BistroBoss;
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Banner from '../Banner/Banner';
+import BistroBoss from '../BistroBoss/BistroBoss';
 import OrderOnline from '../OrdersOnline/OrdersOnline';
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
         <>
             <Banner />
             <OrderOnline />
+            <BistroBoss />
         </>
     );
 };
