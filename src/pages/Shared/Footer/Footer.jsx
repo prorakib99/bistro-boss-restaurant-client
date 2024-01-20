d9c884dd8008f03a109445ae61b79f966f83d132
Md Rakibul Islam <bdsky25@gmail.com>
2024-01-21 02:27:12 +0600

    Fix Footer Spacing and font size Issues

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -5,13 +5,13 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
     return (
         <footer className='text-white'>
-            <div className='w-full md:h-[300px] grid grid-cols-1 md:grid-cols-2 justify-center items-center gap-0'>
-                <div className='bg-gray-800 py-8 w-full flex flex-col items-center justify-center h-full'>
+            <div className='w-full md:h-[40vh] grid grid-cols-1 md:grid-cols-2 justify-center gap-0'>
+                <div className='bg-gray-800 py-8 md:py-16 w-full flex flex-col h-full'>
                     <div className='flex flex-col gap-2 px-5'>
-                        <header className="text-center text-white text-xl sm:text-[32px] mb-4 font-medium font-['Inter']">
+                        <h3 className="text-center text-white text-xl sm:text-[32px] mb-4 font-medium font-['Inter']">
                             CONTACT US
-                        </header>
-                        <div className="text-center sm:flex flex-col gap-3 text-white text-lg sm:text-xl font-medium font-['Inter'] leading-9">
+                        </h3>
+                        <div className="text-center sm:flex flex-col gap-3 text-white text-base sm:text-lg font-medium font-['Inter'] leading-9">
                             <p>123 ABS Street, Uni 21, Bangladesh</p>
                             <p>+88 123456789</p>
                             <p>Mon - Fri: 08:00 - 22:00</p>
@@ -19,22 +19,22 @@ const Footer = () => {
                         </div>
                     </div>
                 </div>
-                <div className='bg-gray-900 py-8 w-full h-full flex flex-col items-center justify-center'>
+                <div className='bg-gray-900 py-8 md:py-16 w-full h-full flex flex-col'>
                     <div className='sm:flex flex-col gap-3 px-5'>
-                        <header className="text-center text-white text-xl sm:text-[32px] mb-4 font-medium font-['Inter']">
+                        <h3 className="text-center text-white text-xl sm:text-[32px] mb-4 font-medium font-['Inter']">
                             Follow US
-                        </header>
-                        <h6 className="text-center text-white text-xl font-medium font-['Inter'] leading-9">
+                        </h3>
+                        <h6 className="text-center text-white text-base sm:text-lg font-medium font-['Inter'] leading-9">
                             Join us on social media
                         </h6>
                         <div className='flex gap-4 items-center mt-2 sm:mt-0 justify-center'>
-                            <Link to='https://www.facebook.com/fxminhaj1002/' target='_blank'>
+                            <Link to='/'>
                                 <FaFacebookF className='w-[35px] h-[35px] relative flex-col justify-start items-start inline-flex' />
                             </Link>
-                            <Link to='https://www.instagram.com/fxminhaj/' target='_blank'>
+                            <Link to='/'>
                                 <FaInstagram className='w-[35px] h-[35px] relative flex-col justify-start items-start inline-flex' />
                             </Link>
-                            <Link to='https://twitter.com/Parsonal32' target='_blank'>
+                            <Link to='/'>
                                 <FaTwitter className='w-[35px] h-[35px] relative flex-col justify-start items-start inline-flex' />
                             </Link>
                         </div>
