993310e75e62a50045495c6dba1a581a440eef2c
Md Rakibul Islam <bdsky25@gmail.com>
2024-01-21 05:17:45 +0600

    Fix Bistro Bos padding issue for mobile device

--- src/pages/Home/BistroBoss/BistroBoss.jsx
+++ src/pages/Home/BistroBoss/BistroBoss.jsx
@@ -7,7 +7,7 @@ const BistroBoss = () => {
             style={{ backgroundImage: `url(${bistroBg})` }}
         >
             <div className='container mx-auto px-5'>
-                <div className=' bg-white lg:w-9/12 p-20 mx-auto'>
+                <div className=' bg-white lg:w-9/12 p-8 lg:p-20 mx-auto'>
                     <h2 className="text-neutral-900 text-center text-[30px] sm:text-[45px] font-normal font-['Cinzel']">
                         Bistro Boss
                     </h2>
