fbc250b0d450fbd5feb08c9a1d8cbe85a477bd44
Md Rakibul Islam <bdsky25@gmail.com>
2024-01-24 04:43:18 +0600

    Implement Loader/Spinner for check User status time

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,8 +1,16 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../pages/Shared/Header/Header';
 import Footer from '../pages/Shared/Footer/Footer';
+import { useContext } from 'react';
+import Loader from '../pages/Shared/Loader/Loader';
+import { AuthContext } from '../providers/AuthProvider';
 
 const Main = () => {
+    const { loading } = useContext(AuthContext);
+
+    if (loading) {
+        return <Loader />;
+    }
     return (
         <>
             <Header></Header>
--- src/pages/Shared/Loader/Loader.jsx
+++ src/pages/Shared/Loader/Loader.jsx
@@ -1,7 +1,7 @@
 const Loader = () => {
     return (
-        <div className='h-screen flex items-center justify-center'>
-            <span className='loading loading-spinner loading-lg'></span>
+        <div className='h-screen w-screen flex items-center justify-center'>
+            <span className='loading loading-bars loading-lg'></span>
         </div>
     );
 };
